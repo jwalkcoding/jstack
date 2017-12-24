9d0088966857b7a98c0db7b9c8b474e760b2337e
Jay Walker <jwalker.nationwide@gmail.com>
2017-12-24 14:55:32 -0500

    Initial commit

---
+++ directives/listview/list.directive.js
@@ -0,0 +1,9 @@
+myApp.directive('listView', function() {
+
+    return {
+
+        templateUrl: '/directives/listview/list.tpl.html',
+        replace: true
+    };
+
+})
---
+++ main.js
@@ -0,0 +1,5 @@
+myApp.controller('mainCtrl', function($scope) {
+
+
+
+});
