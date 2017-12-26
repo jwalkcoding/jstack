b89189d7cb466e6779f3465fa0f6ef1ce9217619
jwalkcoding <jwalker.nationwide@gmail.com>
2017-12-25 19:31:43 -0500

    added repeater

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 myApp.controller('mainCtrl', function($scope) {
 
-
+    $scope.records = [
+        {
+             "Name" : "Alfreds Futterkiste",
+             "Country" : "Germany"
+         },{
+             "Name" : "Berglunds snabbköp",
+             "Country" : "Sweden"
+         },{
+             "Name" : "Centro comercial Moctezuma",
+             "Country" : "Mexico"
+         },{
+             "Name" : "Ernst Handel",
+             "Country" : "Austria"
+         }
+     ]
 
 });
